01d7235ca6a87e06d718df5dba175c324144196a
bathirjsr <simahjsr@gmail.com>
2022-04-15 17:01:56 -0400

    Updated with Shell Check

--- Automization-Codes/Misc/QM_Scan.sh
+++ Automization-Codes/Misc/QM_Scan.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep 'Final converged energy:' $1 | awk 'NR==1{init=$NF}{print NR,($(NF)-init)*627.5095}' > PES.dat
+grep 'Final converged energy:' "$1" | awk 'NR==1{init=$NF}{print NR,($(NF)-init)*627.5095}' > PES.dat
 awk 'prev!=""&&prev<=prev2&&prev<=$2{print line}{prev2=prev;prev=$2;line=$0}' PES.dat | awk '{printf "%2.0f %5.12f \n", $1,$2}' > min.dat
 awk 'prev!=""&&prev>=prev2&&prev>=$2{print line}{prev2=prev;prev=$2;line=$0}' PES.dat | awk '{printf "%2.0f %5.12f \n", $1,$2}' > max.dat
 last=$(awk 'END{print NR}' PES.dat)
@@ -13,16 +13,16 @@ function ActivationEnergy() {
 	echo "Step Energy"
 	cat min.dat
 	echo "Choose the RC (from Minimas):"
-	read rc
+	read -r rc
     echo ""
 	echo "Local Maximas:"
 	echo "Step Energy"
 	cat max.dat
 	echo "Choose the TS (from Maximas):"
-	read ts
+	read -r ts
 	act=$(awk -v r="${rc}" -v t="${ts}" 'NR==r {rc=$(NF)};NR==t {ts=$(NF)};END {printf "%5.12f", (ts-rc)}' PES.dat)
     echo ""
-	echo "Activation Energy for Maxima=${ts}(TS) is: " $act
+	echo "Activation Energy for Maxima=${ts}(TS) is: " "$act"
     echo ""
 }
 
@@ -32,18 +32,18 @@ function ReactionEnergy() {
 	echo "Step Energy"
 	cat min.dat	
 	echo "Choose the RC (from Minimas):"
-	read rc
+	read -r rc
 	echo "Choose the PD (from Minimas):"
-	read pd
+	read -r pd
 	r_act=$(awk -v r="${rc}" -v p="${pd}" 'NR==r {rc=$(NF)};NR==p {pd=$(NF)};END {printf "%5.12f", (pd-rc)}' PES.dat)
     echo ""
-	echo "Reaction Energy for Minima=${ts}(PD) is: " ${r_act}
+	echo "Reaction Energy for Minima=${ts}(PD) is: " "${r_act}"
     echo ""
 }
 
 function Plot() {
 	echo "Give the filename for Potential Energy Surface Plot?  Note: Will be saved as .eps file"
-	read plot
+	read -r plot
 	gnuplot << EOF
 set encoding iso_8859_1
 set term post enhanced eps solid color lw 2.0 "Arial" 24
@@ -59,12 +59,12 @@ plot "PES.dat" u 1:2 t "Energy" w lp pt 7 ps 2 lc rgb "black",\
      "min.dat" u 1:2:(sprintf("(%d)", \$1)) with labels point  pt 7 offset char 1.5,0 notitle,\
      "max.dat" u 1:2:(sprintf("(%d)", \$1)) with labels point  pt 7 offset char 1.5,0 notitle,
 EOF
-	evince ${plot}.eps
+	evince "${plot}".eps
 }
 
 function Exit() {
 	echo "Do you want to keep Max and Min data files? y/n"
-	read tmp
+	read -r tmp
 if [ "$tmp" = "n" ]; then
 	rm PES.dat min.dat max.dat
 	exit 0
@@ -84,10 +84,10 @@ clear='\e[0m'
 ##
 
 ColorGreen(){
-	echo -ne $green$1$clear
+	echo -ne "$green""$1""$clear"
 }
 ColorBlue(){
-	echo -ne $blue$1$clear
+	echo -ne "$blue""$1""$clear"
 }
 
 menu(){
@@ -98,7 +98,7 @@ $(ColorGreen '2)') Reaction Energy
 $(ColorGreen '3)') Plot
 $(ColorGreen '0)') Exit
 $(ColorBlue 'Choose an option:') "
-        read a
+        read -r a
         case $a in
 	       # 1) Minimas ; menu ;;
 	       # 2) Maximas ; menu ;;
@@ -106,9 +106,9 @@ $(ColorBlue 'Choose an option:') "
 	        2) ReactionEnergy ; menu ;;
 	        3) Plot ; menu ;;
 		0) Exit ;;
-		*) echo -e $red"Wrong option."$clear; WrongCommand;;
+		*) echo -e "$red""Wrong option.""$clear"; WrongCommand;;
         esac
 }
-echo -ne "$(ColorGreen 'Last Energy:')" $lastenergy
+echo -ne "$(ColorGreen 'Last Energy:')" "$lastenergy"
 # Call the menu function
 menu
