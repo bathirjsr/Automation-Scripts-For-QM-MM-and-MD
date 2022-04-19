517c9afe65de2a73974249b20d2f5a98c251e52d
bathirjsr <simahjsr@gmail.com>
2022-04-19 13:18:44 -0400

    Removed Current Time

--- Execution_ready/Hbond_Analysis.sh
+++ Execution_ready/Hbond_Analysis.sh
@@ -15,28 +15,28 @@ residinp=$(basename -- "${substrate}")
 residlast="${residinp##*-}"
 residfirst="${residinp%-*}"
 current_time=$(date "+%Y.%m.%d-%H.%M.%S")
-cat > Hbond_analysis_sub_"${current_time}".dat <<EOF
+cat > Hbond_analysis_sub.dat <<EOF
 
 EOF
-cat > Hbond_analysis_"${current_time}".dat <<EOF
+cat > Hbond_analysis.dat <<EOF
 
 EOF
 if [ "${type}" = "0" ]; then
  for i in $(seq "${residfirst}" 1 "${residlast}");
  do
- awk -v i="${i}" '$1 ~ i {print $0}' "${acceptor}" | sort -n >> Hbond_analysis_sub_"${current_time}".dat
- awk -v i="${i}" '$2 ~ i {print $0}' "${donor}" | sort -n >> Hbond_analysis_sub_"${current_time}".dat
+ awk -v i="${i}" '$1 ~ i {print $0}' "${acceptor}" | sort -n >> Hbond_analysis_sub.dat
+ awk -v i="${i}" '$2 ~ i {print $0}' "${donor}" | sort -n >> Hbond_analysis_sub.dat
  done
 
-list=$(awk '{ a[$1]++ } END { for (b in a) { print b } }' Hbond_analysis_sub_"${current_time}".dat )
+list=$(awk '{ a[$1]++ } END { for (b in a) { print b } }' Hbond_analysis_sub.dat )
 list_arr=(${list})
 for i in $list
 do
-	row2=$( awk -v r="$i" '$1==r{print $0}' Hbond_analysis_sub_"${current_time}".dat | awk '{ a[$2]++ } END { for (b in a) { print b } }' )
+	row2=$( awk -v r="$i" '$1==r{print $0}' Hbond_analysis_sub.dat | awk '{ a[$2]++ } END { for (b in a) { print b } }' )
 	for j in $row2
 	do
 		r2r="${j%@*}"
-		awk -v r="$i" -v r2="$r2r" '$1 == r && $2 ~ r2 {sum += $5} END{print r,r2,sum}' Hbond_analysis_sub_"${current_time}".dat > "${i}"_"${j}"_sub.dat
+		awk -v r="$i" -v r2="$r2r" '$1 == r && $2 ~ r2 {sum += $5} END{print r,r2,sum}' Hbond_analysis_sub.dat > "${i}"_"${j}"_sub.dat
 	done
 done
 
@@ -54,19 +54,19 @@ rm hbond_sum_sub.dat
 elif [ "${type}" = "1" ]; then
  for i in ${residues};
  do
- awk -v i="${i}" '$1 ~ i  {print $0}' "${acceptor}" | sort -n >> Hbond_analysis_"${current_time}".dat
- awk -v i="${i}" '$2 ~ i  {print $0}' "${donor}" | sort -n >> Hbond_analysis_"${current_time}".dat
+ awk -v i="${i}" '$1 ~ i  {print $0}' "${acceptor}" | sort -n >> Hbond_analysis.dat
+ awk -v i="${i}" '$2 ~ i  {print $0}' "${donor}" | sort -n >> Hbond_analysis.dat
  done
 
-list=$(awk '{ a[$1]++ } END { for (b in a) { print b } }' Hbond_analysis_sub_"${current_time}".dat )
+list=$(awk '{ a[$1]++ } END { for (b in a) { print b } }' Hbond_analysis_sub.dat )
 list_arr=(${list})
 for i in $list
 do
-	row2=$( awk -v r="$i" '$1==r{print $0}' Hbond_analysis_"${current_time}".dat | awk '{ a[$2]++ } END { for (b in a) { print b } }' )
+	row2=$( awk -v r="$i" '$1==r{print $0}' Hbond_analysis.dat | awk '{ a[$2]++ } END { for (b in a) { print b } }' )
 	for j in $row2
 	do
 		r2r="${j%@*}"
-		awk -v r="$i" -v r2="$r2r" '$1 == r && $2 ~ r2 {sum += $5} END{print r,r2,sum}' Hbond_analysis_"${current_time}".dat > "${i}"_"${j}".dat
+		awk -v r="$i" -v r2="$r2r" '$1 == r && $2 ~ r2 {sum += $5} END{print r,r2,sum}' Hbond_analysis.dat > "${i}"_"${j}".dat
 	done
 done
 
