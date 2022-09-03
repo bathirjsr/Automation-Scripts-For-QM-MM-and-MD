a74d214af80795bf1249e91bc65fa8a4f702ac83
bathirjsr <simahjsr@gmail.com>
2022-09-03 13:03:26 -0400

    Distance cpptraj

---
+++ Execution_ready/Distance.sh
@@ -0,0 +1,27 @@
+if [[ ! -e Analysis ]]; then
+    mkdir Analysis
+elif [[ ! -d Analysis ]]; then
+    echo "Analysis already exists but is not a directory" 1>&2
+fi
+
+declare -a dirs
+i=1
+for d in */
+do
+    dirs[i++]="${d%/}"
+done
+echo "There are ${#dirs[@]} dirs in the current path"
+cd Analysis || exit 
+for((i=1;i<=${#dirs[@]};i++))
+do
+    cpptraj.cuda <<EOF
+parm ../${dirs[i]}/MD/3avr_solv.prmtop
+trajin ../${dirs[i]}/MD/Analysis/6-md_auto.nc
+distance :OY1@O1 :M3L@CM1 out ${dirs[i]}_CM1_dist.dat time 0.02
+distance :OY1@O1 :M3L@CM2 out ${dirs[i]}_CM2_dist.dat time 0.02
+distance :OY1@O1 :M3L@CM3 out ${dirs[i]}_CM3_dist.dat time 0.02
+distance :OY1@O1 :M3L@NZ out ${dirs[i]}_NZ_dist.dat time 0.02
+run 
+exit
+EOF
+done
