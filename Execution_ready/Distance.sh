f2178e300a45869f29b44f28079ba69fdff26fd7
bathirjsr <simahjsr@gmail.com>
2022-09-29 12:27:09 -0400

    Changes

--- Execution_ready/Distance.sh
+++ Execution_ready/Distance.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-if [[ ! -e Analysis ]]; then
-    mkdir Analysis
-elif [[ ! -d Analysis ]]; then
+if [[ ! -e ../Analysis ]]; then
+    mkdir ../Analysis
+elif [[ ! -d ../Analysis ]]; then
     echo "Analysis already exists but is not a directory" 1>&2
 fi
 
@@ -12,16 +12,20 @@ do
     dirs[i++]="${d%/}"
 done
 echo "There are ${#dirs[@]} dirs in the current path"
-cd Analysis || exit 
+cd ../Analysis || exit 
 for((i=1;i<=${#dirs[@]};i++))
 do
     cpptraj.cuda <<EOF
-parm ../${dirs[i]}/MD/3avr_solv.prmtop
-trajin ../${dirs[i]}/MD/Analysis/6-md_auto.nc
-distance :OY1@O1 :M2L@CM1 out ${dirs[i]}_CM1_dist.dat time 0.02
-distance :OY1@O1 :M2L@H1 out ${dirs[i]}_CM2_dist.dat time 0.02
-distance :OY1@O1 :M2L@CM3 out ${dirs[i]}_CM3_dist.dat time 0.02
-distance :OY1@O1 :M2L@NZ out ${dirs[i]}_NZ_dist.dat time 0.02
+parm ../Data/${dirs[i]}/MD/3avr_solv.prmtop
+trajin ../Data/${dirs[i]}/MD/Analysis/6-md_auto.nc
+distance :OY1@O1 :M3L@CM1 out ${dirs[i]}_CM1_dist.dat time 0.02
+angle :FE1 :OY1@O1 :M3L@CM1 out ${dirs[i]}_CM1_ang.dat time 0.02
+distance :OY1@O1 :M3L@H1 out ${dirs[i]}_CM2_dist.dat time 0.02
+angle :FE1 :OY1@O1 :M3L@CM2 out ${dirs[i]}_CM2_ang.dat time 0.02
+distance :OY1@O1 :M3L@CM3 out ${dirs[i]}_CM3_dist.dat time 0.02
+angle :FE1 :OY1@O1 :M3L@CM3 out ${dirs[i]}_CM3_ang.dat time 0.02
+distance :OY1@O1 :M3L@NZ out ${dirs[i]}_NZ_dist.dat time 0.02
+angle :FE1 :OY1@O1 :M3L@NZ out ${dirs[i]}_NZ_ang.dat time 0.02
 run 
 exit
 EOF
