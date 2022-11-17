2fe9328e8ebf797138a8b6edccbdc7bad0f4f807
bathirjsr <simahjsr@gmail.com>
2022-11-17 16:00:09 -0500

    commit

--- Execution_ready/Less_traj.sh
+++ Execution_ready/Less_traj.sh
@@ -9,14 +9,14 @@ done
 echo "There are ${#dirs[@]} dirs in the current path"
 for((i=1;i<=${#dirs[@]};i++))
 do
-cd "${dirs[i]}"/MD/6-md/ || continue 1
+cd "${dirs[i]}"/MD/Analysis/ || continue 1
     cpptraj.cuda <<EOF
-parm ../3avr_SO_solv.prmtop
-trajin ../Analysis/6-md_auto.nc 1 50000 5
+parm ../EFE_solv.prmtop
+trajin 6-md_auto.nc 1 50000 5
 trajout 6-md_less.nc
 run 
 clear all
-parm ../3avr_SO_solv.prmtop
+parm ../EFE_solv.prmtop
 trajin 6-md_less.nc 5001 10000
 autoimage
 trajout 6-md_mmpbsa.nc
