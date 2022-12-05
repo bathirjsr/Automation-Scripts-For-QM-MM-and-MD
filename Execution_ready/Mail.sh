f3a2756b69955d518acfb8fcb29d8069161fab7a
bathirjsr <simahjsr@gmail.com>
2022-12-05 18:31:49 -0500

    Edited

--- Execution_ready/Mail.sh
+++ Execution_ready/Mail.sh
@@ -1,8 +1,17 @@
 #! /bin/bash
-
+echo "Favorites: simahjsr@gmail.com mgeorget@mtu.edu sdevadas@mtu.edu bvarada@mtu.edu"
 read -re -p "To: " target 
 read -re -p "Subject:" subject
 read -re -p "Content:" content
 read -re -p "Attachment(s):" attachment
 
-echo "$content" | mail -s "${subject}" -a $attachment $target
+declare -a attach
+i=1
+for d in $attachment
+do
+    attach[i++]="-a ${d%/}"
+done
+echo ${attach[*]}
+
+echo "$content" | mail -s "${subject}"  ${attach[*]} "$target"
+echo "Mail Sent"
