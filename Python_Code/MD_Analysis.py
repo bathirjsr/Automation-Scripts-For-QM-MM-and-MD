bc746dd1cabb4b26e47fbc4d68379531a65ed4d3
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2023-09-12 17:01:33 -0400

    Python Scripts for practise

---
+++ Python_Code/MD_Analysis.py
@@ -0,0 +1,15 @@
+import os
+import tkinter as tk
+from tkinter import filedialog
+root=tk.Tk()
+root.withdraw
+if not os.path.exists(Analysis):
+    os.makedirs(Analysis)
+    print(f"Directory 'Analysis' created")
+    os.chdir(Analysis)
+def Autoimage()
+    parm = filedialog.askopenfilename(title="Select a Parameter file")
+    trajin = filedialog.askopenfilename(title="Select a trajectory file")
+
+
+    
