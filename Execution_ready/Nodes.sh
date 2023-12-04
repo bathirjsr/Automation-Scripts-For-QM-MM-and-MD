a686336798e8ad50b074df834eb199adce2a3ada
bathirjsr <simahjsr@gmail.com>
2023-12-04 12:07:38 -0500

    Nodes.sh updated with log file checking

--- Execution_ready/Nodes.sh
+++ Execution_ready/Nodes.sh
@@ -4,11 +4,14 @@ if [ "$1" = "chemsh.x" ]; then
 sum=0  
 for dir in $(pwdx $(pidof chemsh.x) | awk '{print $NF}'); do
     file="$dir/input.in"
+    file2="$dir/dscf.log"
     if [ -e "$file" ]; then
         line=$(grep "nodes" "$file")
+        check=$(tail -2 $file2)
         if [ -n "$line" ]; then
             echo "In directory $dir:"
             echo "$line"
+            echo "$check"
             sum=$((sum + line))
         fi
     fi
