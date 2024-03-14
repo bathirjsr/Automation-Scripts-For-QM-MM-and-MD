badbdc58ca60f347b9d1ba5acef1c9e71a9993ab
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2024-03-14 16:15:53 -0400

    variable fix

--- Execution_ready/EF_New.sh
+++ Execution_ready/EF_New.sh
@@ -12,61 +12,61 @@ done
 
 if [ "$combo" = "FE-O" ]; then
 mkdir EF_${combo}
-cp "${step,,}".opt.pdb EF_${combo}/.
-cp "${step,,}".prmtop EF_${combo}/.
+cp ${step,,}.opt.pdb EF_${combo}/.
+cp ${step,,}.prmtop EF_${combo}/.
 cd EF_${combo} || exit
-x1=$(awk '$4 == "FE1" && $3 == "FE" {print $6}' "${step,,}".opt.pdb)
-y1=$(awk '$4 == "FE1" && $3 == "FE" {print $7}' "${step,,}".opt.pdb)
-z1=$(awk '$4 == "FE1" && $3 == "FE" {print $8}' "${step,,}".opt.pdb)
+x1=$(awk '$4 == "FE1" && $3 == "FE" {print $6}' ${step,,}.opt.pdb)
+y1=$(awk '$4 == "FE1" && $3 == "FE" {print $7}' ${step,,}.opt.pdb)
+z1=$(awk '$4 == "FE1" && $3 == "FE" {print $8}' ${step,,}.opt.pdb)
 
-x2=$(awk '$4 == "OY1" && $3 == "O1" {print $6}' "${step,,}".opt.pdb)
-y2=$(awk '$4 == "OY1" && $3 == "O1" {print $7}' "${step,,}".opt.pdb)
-z2=$(awk '$4 == "OY1" && $3 == "O1" {print $8}' "${step,,}".opt.pdb)
+x2=$(awk '$4 == "OY1" && $3 == "O1" {print $6}' ${step,,}.opt.pdb)
+y2=$(awk '$4 == "OY1" && $3 == "O1" {print $7}' ${step,,}.opt.pdb)
+z2=$(awk '$4 == "OY1" && $3 == "O1" {print $8}' ${step,,}.opt.pdb)
 elif [ "$combo" = "Op-C1" ]; then
 mkdir EF_${combo}
-cp "${step,,}".opt.pdb EF_${combo}/.
-cp "${step,,}".prmtop EF_${combo}/.
+cp ${step,,}.opt.pdb EF_${combo}/.
+cp ${step,,}.prmtop EF_${combo}/.
 cd EF_${combo} || exit
-x1=$(awk '$4 == "OY1" && $3 == "O1" {print $6}' "${step,,}".opt.pdb)
-y1=$(awk '$4 == "OY1" && $3 == "O1" {print $7}' "${step,,}".opt.pdb)
-z1=$(awk '$4 == "OY1" && $3 == "O1" {print $8}' "${step,,}".opt.pdb)
+x1=$(awk '$4 == "OY1" && $3 == "O1" {print $6}' ${step,,}.opt.pdb)
+y1=$(awk '$4 == "OY1" && $3 == "O1" {print $7}' ${step,,}.opt.pdb)
+z1=$(awk '$4 == "OY1" && $3 == "O1" {print $8}' ${step,,}.opt.pdb)
 
-x2=$(awk '$4 == "AG1" && $3 == "C1" {print $6}' "${step,,}".opt.pdb)
-y2=$(awk '$4 == "AG1" && $3 == "C1" {print $7}' "${step,,}".opt.pdb)
-z2=$(awk '$4 == "AG1" && $3 == "C1" {print $8}' "${step,,}".opt.pdb)
+x2=$(awk '$4 == "AG1" && $3 == "C1" {print $6}' ${step,,}.opt.pdb)
+y2=$(awk '$4 == "AG1" && $3 == "C1" {print $7}' ${step,,}.opt.pdb)
+z2=$(awk '$4 == "AG1" && $3 == "C1" {print $8}' ${step,,}.opt.pdb)
 elif [ "$combo" = "Od-C2" ]; then
 mkdir EF_${combo}
-cp "${step,,}".opt.pdb EF_${combo}/.
-cp "${step,,}".prmtop EF_${combo}/.
+cp ${step,,}.opt.pdb EF_${combo}/.
+cp ${step,,}.prmtop EF_${combo}/.
 cd EF_${combo} || exit
-x1=$(awk '$4 == "OY1" && $3 == "O2" {print $6}' "${step,,}".opt.pdb)
-y1=$(awk '$4 == "OY1" && $3 == "O2" {print $7}' "${step,,}".opt.pdb)
-z1=$(awk '$4 == "OY1" && $3 == "O2" {print $8}' "${step,,}".opt.pdb)
+x1=$(awk '$4 == "OY1" && $3 == "O2" {print $6}' ${step,,}.opt.pdb)
+y1=$(awk '$4 == "OY1" && $3 == "O2" {print $7}' ${step,,}.opt.pdb)
+z1=$(awk '$4 == "OY1" && $3 == "O2" {print $8}' ${step,,}.opt.pdb)
 
-x2=$(awk '$4 == "AG1" && $3 == "C2" {print $6}' "${step,,}".opt.pdb)
-y2=$(awk '$4 == "AG1" && $3 == "C2" {print $7}' "${step,,}".opt.pdb)
-z2=$(awk '$4 == "AG1" && $3 == "C2" {print $8}' "${step,,}".opt.pdb)
+x2=$(awk '$4 == "AG1" && $3 == "C2" {print $6}' ${step,,}.opt.pdb)
+y2=$(awk '$4 == "AG1" && $3 == "C2" {print $7}' ${step,,}.opt.pdb)
+z2=$(awk '$4 == "AG1" && $3 == "C2" {print $8}' ${step,,}.opt.pdb)
 elif [ "$combo" = "C1-C2" ]; then
 mkdir EF_${combo}
-cp "${step,,}".opt.pdb EF_${combo}/.
-cp "${step,,}".prmtop EF_${combo}/.
+cp ${step,,}.opt.pdb EF_${combo}/.
+cp ${step,,}.prmtop EF_${combo}/.
 cd EF_${combo} || exit
-x1=$(awk '$4 == "AG1" && $3 == "C1" {print $6}' "${step,,}".opt.pdb)
-y1=$(awk '$4 == "AG1" && $3 == "C1" {print $7}' "${step,,}".opt.pdb)
-z1=$(awk '$4 == "AG1" && $3 == "C1" {print $8}' "${step,,}".opt.pdb)
+x1=$(awk '$4 == "AG1" && $3 == "C1" {print $6}' ${step,,}.opt.pdb)
+y1=$(awk '$4 == "AG1" && $3 == "C1" {print $7}' ${step,,}.opt.pdb)
+z1=$(awk '$4 == "AG1" && $3 == "C1" {print $8}' ${step,,}.opt.pdb)
 
-x2=$(awk '$4 == "AG1" && $3 == "C2" {print $6}' "${step,,}".opt.pdb)
-y2=$(awk '$4 == "AG1" && $3 == "C2" {print $7}' "${step,,}".opt.pdb)
-z2=$(awk '$4 == "AG1" && $3 == "C2" {print $8}' "${step,,}".opt.pdb)
+x2=$(awk '$4 == "AG1" && $3 == "C2" {print $6}' ${step,,}.opt.pdb)
+y2=$(awk '$4 == "AG1" && $3 == "C2" {print $7}' ${step,,}.opt.pdb)
+z2=$(awk '$4 == "AG1" && $3 == "C2" {print $8}' ${step,,}.opt.pdb)
 
 fi
-cp "${step,,}".opt.pdb "${step,,}".opt.pdb.bk
+cp ${step,,}.opt.pdb ${step,,}.opt.pdb.bk
 
 
 cat > cpptraj.in <<ENDOFFILE
-parm "${step,,}".prmtop
-trajin "${step,,}".opt.pdb
-reference "${step,,}".opt.pdb
+parm ${step,,}.prmtop
+trajin ${step,,}.opt.pdb
+reference ${step,,}.opt.pdb
 strip !(:${nt}-${ct}) outprefix std
 trajout std.pdb
 run
