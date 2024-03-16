a9232ef9c4df4222c0e16690768957fc867ce1d7
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2024-03-16 16:28:05 -0400

    Commit

--- Execution_ready/EF_New.sh
+++ Execution_ready/EF_New.sh
@@ -12,7 +12,7 @@ done
 if [ "$step" = "scan" ]; then
 ls -v *.pdb
 read -p "Total number of structures{Remeber should have scan_0.pdb file in the current directory}:" $count
-for i in $(seq 0 $count); do
+for i in $(seq 0 ${count}); do
 
 if [ "$combo" = "FE-O" ]; then
     mkdir EF_${combo}_${i}
