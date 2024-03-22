be6a65bdb8a4b33bf40f40b7cb30680b04a8db84
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2024-03-22 16:21:10 -0400

    vmd visualization

---
+++ Execution_ready/Vmd_rep.sh
@@ -0,0 +1,21 @@
+while getopts c:p:r: flag
+do
+    case "${flag}" in
+	c) pdb=${OPTARG};;
+	p) prmtop=${OPTARG};;
+    r) representation=${OPTARG};;
+esac
+done
+
+cat > DCCA_Visual_${i}.dat << EOF
+mol new ${prmtop}
+mol addfile ${pdb}
+mol delrep 0 top
+mol selection ${representation}
+mol representation CPK
+mol addrep top
+mol representation NewCartoon 0.1 20
+mol addrep top
+mol modstyle 2 0 0.5
+EOF
+vmd < DCCA_Visual_${i}.dat
