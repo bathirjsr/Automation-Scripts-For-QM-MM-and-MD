406d13b808f2d6c45f181819be5c1e959381bb24
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2024-03-22 16:47:31 -0400

    Commit

--- Execution_ready/Vmd_rep.sh
+++ Execution_ready/Vmd_rep.sh
@@ -11,9 +11,9 @@ cat > DCCA_Visual_${i}.dat << EOF
 mol new ${prmtop}
 mol addfile ${pdb}
 mol delrep 0 top
-mol selection resname FE1 OY1 HD1 HD2 AP1 AG1 SC1 Cl1 ${substrate}
+mol selection resname FE1 OY1 O11 HD1 HD2 AP1 GU1 AG1 SC1 Cl1 ${substrate}
 mol representation CPK
 mol addrep top
-
+animate center
 EOF
 vmd -e DCCA_Visual_${i}.dat
