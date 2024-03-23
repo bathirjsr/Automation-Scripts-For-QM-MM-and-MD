afcbf37ccb4b2986751e969510c230d76e4d0d9e
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2024-03-23 14:34:14 -0400

    Commit

--- Execution_ready/Vmd_rep.sh
+++ Execution_ready/Vmd_rep.sh
@@ -9,7 +9,7 @@ done
 filename="${coordinate##*/}"
 filext="${filename##*.}"
 file="${filename%.*}"
-vmd << EOF 
+cat > vmd.tcl << EOF
 set file $file
 set ext  $filext
 if { \$ext == "pdb"} {
@@ -19,6 +19,8 @@ if { \$ext == "pdb"} {
     mol selection resname FE1 OY1 O11 HD1 HD2 AP1 GU1 AG1 SC1 Cl1 ${substrate}
     mol representation CPK
     mol addrep top
+    animate center
+    display set bg_color white
 } elseif { \$ext == "nc"} {
     mol new ${prmtop}
     mol addfile ${coordinate} first 0 last -1 step 1 waitfor all
@@ -30,3 +32,4 @@ if { \$ext == "pdb"} {
     puts "Unsupported file type: $ext"
 }
 EOF
+vmd -e vmd.tcl
