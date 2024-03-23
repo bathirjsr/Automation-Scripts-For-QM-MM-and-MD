3eb50a67ea2d9d52655524856a54fdedf123990f
Simahudeen Bathir JSR <84030364+bathirjsr@users.noreply.github.com>
2024-03-23 13:39:02 -0400

    updated to include .nc file in Vmd_rep.sh

--- Execution_ready/Vmd_rep.sh
+++ Execution_ready/Vmd_rep.sh
@@ -1,19 +1,35 @@
 while getopts c:p:s: flag
 do
     case "${flag}" in
-	c) pdb=${OPTARG};;
+	c) coordinate=${OPTARG};;
 	p) prmtop=${OPTARG};;
     s) substrate=${OPTARG};;
 esac
 done
-
+filename="${coordinate##*/}"
+filext="${filename##*.}"
+file="${filename%.*}"
 cat > DCCA_Visual_${i}.dat << EOF
-mol new ${prmtop}
-mol addfile ${pdb}
-mol delrep 0 top
-mol selection resname FE1 OY1 O11 HD1 HD2 AP1 GU1 AG1 SC1 Cl1 ${substrate}
-mol representation CPK
-mol addrep top
-animate center
+set file "$file"
+set ext  "$filext"
+if {$ext == ".pdb"} {
+    mol new ${prmtop}
+    mol load pdb ${file}${ext}
+    mol delrep 0 top
+    mol selection resname FE1 OY1 O11 HD1 HD2 AP1 GU1 AG1 SC1 Cl1 ${substrate}
+    mol representation CPK
+    mol addrep top
+    animate center
+    display set bg_color white
+} elseif {$ext == ".nc"} {
+    mol new ${prmtop}
+    mol addfile ${file}${ext} type nc first 0 last -1 step 1 waitfor all
+    mol selection resname FE1 OY1 O11 HD1 HD2 AP1 GU1 AG1 SC1 Cl1 ${substrate}
+    mol representation CPK
+    mol addrep top
+} else {
+    # Handle other file types or show an error/message
+    puts "Unsupported file type: $ext"
+}
 EOF
 vmd -e DCCA_Visual_${i}.dat
