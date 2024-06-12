a3ab03ba78d356f8c03ce0ea7f7e9e1fdacfc869
bathirjsr <simahjsr@gmail.com>
2024-06-12 12:18:09 -0400

    variable input for number of atoms in NO_SNO

--- Execution_ready/NO_SNO_EFE.sh
+++ Execution_ready/NO_SNO_EFE.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# This script is used to generate Natural Orbital and Spin Natural Orbital Occupancy calculations
+while getopts n: flag
+do
+     case "${flag}" in
+ 	    n) n=${OPTARG};;
+ 	    *) echo "usage: $0 [-n number of atoms in QM] " >&2
+      exit 1 ;;
+     esac
 if [ "$1" = "RC" ]
 then
   mkdir NO_SNO
@@ -12,7 +20,7 @@ then
   echo "" >> beta_edit
   cp ../SP/hybrid.turbomole.coords .
   #######################Change sed number here to total number of atoms###############
-  sed '1,4d' hybrid.turbomole.coords | sed -n '1,78 p' | awk '{printf "%2s%16f%14f%14f\n", $1,$2*0.529177249,$3*0.529177249,$4*0.529177249}' > inpcrd
+  sed '1,4d' hybrid.turbomole.coords | sed -n "1,$n p" | awk '{printf "%2s%16f%14f%14f\n", $1,$2*0.529177249,$3*0.529177249,$4*0.529177249}' > inpcrd
   echo "" >> inpcrd
 
 ####################Change Charge and Multiplicity here#################
@@ -32,7 +40,7 @@ EOF
   cp RC_NO.chk RC_SNO.chk
   sed -i 's/RC_NO/RC_SNO/g;s/NaturalOrbitals/SpinNatural/g' RC_SNO.com
   nohup g16 < RC_SNO.com > RC_SNO.log
-grep 'Eigenvalues' RC_NO.log |awk '{ if (NF==7) print $(NF-4),$(NF-3),$(NF-2),$(NF-1),$NF; else if (NF==6) print $(NF-3),$(NF-2),$(NF-1),$NF; else if (NF==5) print $(NF-2),$(NF-1),$NF; else if (NF==4) print $(NF-1),$NF; else if (NF==3) print $NF}' | awk '{ for(i=1;i<=NF;i++) print $i; }' | awk '{print NR,$0}' > NO_Occupancy.dat
+  grep 'Eigenvalues' RC_NO.log |awk '{ if (NF==7) print $(NF-4),$(NF-3),$(NF-2),$(NF-1),$NF; else if (NF==6) print $(NF-3),$(NF-2),$(NF-1),$NF; else if (NF==5) print $(NF-2),$(NF-1),$NF; else if (NF==4) print $(NF-1),$NF; else if (NF==3) print $NF}' | awk '{ for(i=1;i<=NF;i++) print $i; }' | awk '{print NR,$0}' > NO_Occupancy.dat
   grep 'Eigenvalues' RC_SNO.log |awk '{ if (NF==7) print $(NF-4),$(NF-3),$(NF-2),$(NF-1),$NF; else if (NF==6) print $(NF-3),$(NF-2),$(NF-1),$NF; else if (NF==5) print $(NF-2),$(NF-1),$NF; else if (NF==4) print $(NF-1),$NF; else if (NF==3) print $NF}' | awk '{ for(i=1;i<=NF;i++) print $i; }' | awk '{print NR,$0}' > SNO_Occupancy.dat
 
 elif [ "$1" = "TS" ]
@@ -47,7 +55,7 @@ then
   sed '1,3d' beta | sed '$d' > beta_edit
   echo "" >> beta_edit
   cp ../SP/hybrid.turbomole.coords .
-  sed '1,4d' hybrid.turbomole.coords | sed -n '1,78 p' | awk '{printf "%2s%16f%14f%14f\n", $1,$2*0.529177249,$3*0.529177249,$4*0.529177249}' > inpcrd
+  sed '1,4d' hybrid.turbomole.coords | sed -n "1,$n p" | awk '{printf "%2s%16f%14f%14f\n", $1,$2*0.529177249,$3*0.529177249,$4*0.529177249}' > inpcrd
   echo "" >> inpcrd
   cat > Gauss_code.txt << EOF
 %nproc=12
@@ -79,8 +87,8 @@ then
   cp ../SP/beta .
   sed '1,3d' beta | sed '$d' > beta_edit
   echo "" >> beta_edit
-  cp ../SP/hybrid.turbomole.coords .
-  sed '1,4d' hybrid.turbomole.coords | sed -n '1,78 p' | awk '{printf "%2s%16f%14f%14f\n", $1,$2*0.529177249,$3*0.529177249,$4*0.529177249}' > inpcrd
+  cp ../hybrid.turbomole.coords .
+  sed '1,4d' hybrid.turbomole.coords | sed -n "1,$n p" | awk '{printf "%2s%16f%14f%14f\n", $1,$2*0.529177249,$3*0.529177249,$4*0.529177249}' > inpcrd
   echo "" >> inpcrd
   cat > Gauss_code.txt << EOF
 %nproc=12
