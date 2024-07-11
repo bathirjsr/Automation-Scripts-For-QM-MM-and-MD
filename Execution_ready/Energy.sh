3882facce27ce800b475f4ce8c3de03f25299022
bathirjsr <simahjsr@gmail.com>
2024-07-11 11:04:22 -0400

    Commit

--- Execution_ready/Energy.sh
+++ Execution_ready/Energy.sh
@@ -4,6 +4,7 @@ then
 B1=$(grep 'Final converged energy' ${1}_dlfind.log|awk '{printf "%5.12f", $NF}')
 elif [ "$1" = "TS" ]
 then
+B1=$(grep 'Final converged energy' ${1}_Opt.log|awk '{printf "%5.12f", $NF}')
 grep 'frequencies' Frequency/TS_Freq.log | head -n 4
 else
 B1=$(grep 'Final converged energy' ${1}_Opt.log|awk '{printf "%5.12f", $NF}')
